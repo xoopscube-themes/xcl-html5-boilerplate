5ec6b206bd3f12981b75f653efeebd5ef83fa4eb
gigamaster <1905497+gigamaster@users.noreply.github.com>
2022-05-19 21:00:03 +0200

    Update HTML5 Boilerplate v8.0.0 rc2

---
+++ html/themes/xcl-html5-boilerplate/js/app.js
@@ -0,0 +1,13 @@
+$(function () {
+
+    // Do something on document ready
+    // Inline SVG icons ex. image with class="svg" :
+    // <img class="svg" src="..." width="24px" height="24px" alt="...">
+    $('.svg').renderClassSvg();
+
+    /* ---------------------------------------*/
+
+// Place any jQuery/helper plugins in here.
+
+    /* ---------------------------------------*/
+});
---
+++ html/themes/xcl-html5-boilerplate/manifesto.ini.php
@@ -0,0 +1,21 @@
+<?php
+/**
+
+[Manifesto]
+Name="HTML5 Boilerplate"
+Depends=Legacy_RenderSystem,legacy
+Url="https://github.com/xoopscube/xcl"
+Version="2.3.1"
+
+[Theme]
+RenderSystem=Legacy_RenderSystem
+Format="XOOPS2 Legacy Style"
+Author=Gigamaster
+ScreenShot="screenshot.png"
+Description="XCL Theme html5 boilerplate (v8.0.0-RC2) for humans and machines to build simple, secure, and scalable responsive web apps and sites."
+W3C=NG
+
+Licence="MIT"
+
+*/
+
